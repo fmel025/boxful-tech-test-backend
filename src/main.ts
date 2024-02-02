bbe3efd8584f3e842175185516ab859c2e7e688e
fmel025 <00018720@uca.edu.sv>
2024-02-02 09:42:38 -0600

    🔧 feat: Set up Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Boxful API')
+    .setDescription('Boxful Tech Test API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
+  // PORT settings
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   await app.listen(port);
