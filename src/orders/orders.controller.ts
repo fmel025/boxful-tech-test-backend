3a8c0867ebbd98515e54a066f1319ff4f2867c30
fmel025 <00018720@uca.edu.sv>
2024-02-02 13:25:33 -0600

    ✨ feat(orders): Add services to save and get orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,16 +1,10 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('orders')
+@ApiTags('Orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order, OrderDocument } from './schemas/order.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class OrdersService {
+  constructor(
+    @InjectModel(Order.name) private readonly orderModel: Model<OrderDocument>,
+  ) {}
+
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    return this.orderModel.create(createOrderDto);
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll() {
+    return this.orderModel.find({});
   }
 }
