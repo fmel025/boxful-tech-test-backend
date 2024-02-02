7bb3d2755c1c56f99dde5019da9356c42ce4c774
fmel025 <00018720@uca.edu.sv>
2024-02-02 10:28:44 -0600

    🔥 refactor(orders): Remove unused code

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -23,20 +22,5 @@ export class OrdersController {
   @Get()
   findAll() {
     return this.ordersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
   }
 }
