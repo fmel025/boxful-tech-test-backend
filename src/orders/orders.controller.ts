2a29aaccadfe9e339d38d3f8575cfba26282d271
fmel025 <00018720@uca.edu.sv>
2024-02-02 14:14:06 -0600

    📝 docs: Updated swagger documentation

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,18 +1,41 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
 
 @Controller('orders')
 @ApiTags('Orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @ApiOperation({
+    description: 'It creates a new order',
+    summary: 'Create a new order',
+  })
+  @ApiCreatedResponse({
+    description: 'Order created successfully',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid body data',
+  })
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @ApiOperation({
+    description: 'It retrieves all the orders',
+    summary: 'Get all the orders',
+  })
+  @ApiOkResponse({
+    description: 'Orders retrieved successfully',
+  })
   @Get()
   findAll() {
     return this.ordersService.findAll();
