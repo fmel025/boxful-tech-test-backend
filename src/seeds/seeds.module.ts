2259b5bed2f82d5bfc54488181b347ff35c4e265
fmel025 <00018720@uca.edu.sv>
2024-02-02 19:10:24 -0600

    ✨ feat: Import State schema to seed module

--- src/seeds/seeds.module.ts
+++ src/seeds/seeds.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
 import { SeedsController } from './seeds.controller';
+import { StatesModule } from '@States/states.module';
 
 @Module({
+  imports: [StatesModule],
   controllers: [SeedsController],
   providers: [SeedsService],
 })
