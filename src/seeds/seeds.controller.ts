8c7db5fa7c624ad89c4f53af35cb58fd56b3f4a2
fmel025 <00018720@uca.edu.sv>
2024-02-02 18:07:44 -0600

    📝 docs(swagger): Add documentation to seeds and states controllers

--- src/seeds/seeds.controller.ts
+++ src/seeds/seeds.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
+import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('seeds')
+@ApiTags('Seeds')
 export class SeedsController {
   constructor(private readonly seedsService: SeedsService) {}
 
   @HttpCode(201)
+  @ApiOperation({
+    description: 'This route populates the states collection',
+    summary: 'Populate the states collection'
+  })
+  @ApiCreatedResponse({
+    description: 'State collection has been populated successfully',
+  })
   @Get('states')
   populateStates() {
     return this.seedsService.populateStates();
--- src/states/states.controller.ts
+++ src/states/states.controller.ts
@@ -1,15 +1,44 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { StatesService } from './states.service';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiParam,
+} from '@nestjs/swagger';
 
+@ApiTags('States')
 @Controller('states')
 export class StatesController {
   constructor(private readonly statesService: StatesService) {}
 
+  @ApiOperation({
+    description:
+      'This route gets all the states, including their cities and collection points',
+    summary: 'Get all the states',
+  })
+  @ApiOkResponse({
+    description: 'All states have been found',
+  })
   @Get()
   findAll() {
     return this.statesService.findAll();
   }
 
+  @ApiOperation({
+    description:
+      'This route gets a single state with its cities and collection points',
+    summary: 'Get state by id',
+  })
+  @ApiOkResponse({
+    description: 'State has been found successfully',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.statesService.findOne(id);
