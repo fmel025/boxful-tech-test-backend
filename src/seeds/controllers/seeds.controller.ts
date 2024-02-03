fbc0a3b01a77d6006d60820e9b9d858d3db81fee
fmel025 <00018720@uca.edu.sv>
2024-02-02 22:04:56 -0600

    ✨ feat(seeds): Add populate data method

--- src/seeds/controllers/seeds.controller.ts
+++ src/seeds/controllers/seeds.controller.ts
@@ -1,6 +1,12 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { SeedsService } from '../services/seeds.service';
-import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiCreatedResponse,
+  ApiConflictResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
 @Controller('seeds')
 @ApiTags('Seeds')
@@ -10,10 +16,16 @@ export class SeedsController {
   @HttpCode(201)
   @ApiOperation({
     description: 'This route populates the states collection',
-    summary: 'Populate the states collection'
+    summary: 'Populate the states collection',
   })
   @ApiCreatedResponse({
     description: 'State collection has been populated successfully',
+  })
+  @ApiConflictResponse({
+    description: 'State collection data already exists',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Something went wrong',
   })
   @Get('states')
   populateStates() {
--- src/seeds/services/seeds.service.ts
+++ src/seeds/services/seeds.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { State, StateDocument } from '@States/schemas/state.schema';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { statesData } from '@Common/data';
 
 @Injectable()
 export class SeedsService {
-  populateStates() {
-    return `This action populates the state collection`;
+  constructor(
+    @InjectModel(State.name) private readonly stateModel: Model<StateDocument>,
+  ) {}
+
+  async populateStates() {
+    const statesDocuments = await this.stateModel.find({});
+    const states = statesDocuments.map((state) => state.name);
+    const areStatesDuplicated = statesData.some((state) => {
+      return states.includes(state.name);
+    });
+
+    if (areStatesDuplicated)
+      throw new ConflictException('The states already exists on the database');
+
+    try {
+      await this.stateModel.insertMany(statesData);
+      return 'The states collection has been populated successfully';
+    } catch (error) {
+      throw new InternalServerErrorException('Oops, something went wrong');
+    }
   }
 }
