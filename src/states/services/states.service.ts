349bd15f9245f8368a6e4d47c33e49d0c7710e52
fmel025 <00018720@uca.edu.sv>
2024-02-02 19:43:29 -0600

    ✨ feat: Add find methods for state service

--- src/states/services/states.service.ts
+++ src/states/services/states.service.ts
@@ -1,12 +1,19 @@
+import { State } from '@States/schemas/state.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class StatesService {
-  findAll() {
-    return `This action returns all states`;
+  constructor(
+    @InjectModel(State.name) private readonly stateModel: Model<State>,
+  ) {}
+
+  async findAll(): Promise<State[]> {
+    return await this.stateModel.find({});
   }
 
-  findOne(id: string) {
-    return `This action returns a single state`;
+  async findOne(id: string): Promise<State> {
+    return await this.stateModel.findById(id);
   }
 }
